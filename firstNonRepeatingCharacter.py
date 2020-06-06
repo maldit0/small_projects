06b851cdd0f0691fc6c7705e199fd8cf088393cf
luciano.montanares@inacapmail.cl <qpwoeiaf1a2s3d2s1a>
2020-06-06 03:21:01 -0400

    update .gitignore

--- firstNonRepeatingCharacter.py
+++
@@ -1,9 +0,0 @@
-print("Enter two numbers. I'll divide them.")
-x = input("First number: ")
-y = input("Second number: ")
-try:
-    result = int(x) / int(y)
-except ZeroDivisionError:
-    print("You can't divide by zero!")
-else:
-    print(result)
